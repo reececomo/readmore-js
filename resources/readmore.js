f64ccf318f7f3d04104aa25dcdfcffd93784a3a2
Reece Como <reece@hubr.io>
2018-01-09 16:16:45 +0800

    Update readmore.js

--- resources/readmore.js
+++ resources/readmore.js
@@ -6,27 +6,31 @@
  * Expanded from: "Text Fade Out / Read More Link | CSS-Tricks"
  * https://css-tricks.com/text-fade-read-more/
  */
-$(".read-more .button").each(function() {
-    // Get objects
-    var thisButton = $(this);
-    var $prompt = thisButton.parent();
-    var $container = $prompt.parent();
+$(document).ready(function() {
 
-    // Get the height of the container
-    var totalHeight = $container[0].scrollHeight;
+    $(".read-more .button").each(function() {
+        // Get objects
+        var thisButton = $(this);
+        var $prompt = thisButton.parent();
+        var $container = $prompt.parent();
 
-    if(totalHeight < 150) {
-        // Too small initially
-        $container.css('max-height', 'none');
-        $prompt.hide();
-    }
+        // Get the height of the container
+        var totalHeight = $container[0].scrollHeight;
 
-    thisButton.on('click', function() {
-        // Set height to prevent instant jump-down when max height is removed
-        $container.css({ "height": $container.height(), "max-height": 9999 }).animate({ "height": totalHeight });
-        
-        $prompt.fadeOut(); // fade out read-more prompt
-        
-        return false; // prevent jump-down
+        if(totalHeight < 150) {
+            // Too small initially
+            $container.css('max-height', 'none');
+            $prompt.hide();
+        }
+
+        thisButton.on('click', function() {
+            // Set height to prevent instant jump-down when max height is removed
+            $container.css({ "height": $container.height(), "max-height": 9999 }).animate({ "height": totalHeight });
+
+            $prompt.fadeOut(); // fade out read-more prompt
+
+            return false; // prevent jump-down
+        });
     });
+
 });
