2b31e338f1bd1457bbbb5165ddc865b6412e41e2
Reece Como <reece@hubr.io>
2018-01-10 11:11:23 +0800

    rm buttons equipment, use prompt for all

--- resources/readmore.js
+++ resources/readmore.js
@@ -1,18 +1,17 @@
-/*
- * Read More Plugin
- *  by Reece Como
- *  9 Jan 2018
- *
- * Expanded from: "Text Fade Out / Read More Link | CSS-Tricks"
- * https://css-tricks.com/text-fade-read-more/
- */
 $(document).ready(function() {
     
-    $(".read-more .button").each(function() {
+    /*
+     * Read More Plugin
+     *  by Reece Como
+     *  9 Jan 2018
+     *
+     * Expanded from: "Text Fade Out / Read More Link | CSS-Tricks"
+     * https://css-tricks.com/text-fade-read-more/
+     */
+    $(".read-more .prompt").each(function() {
         // Get objects
         var $self = $(this);
-        var $prompt = $self.parent();
-        var $container = $prompt.parent();
+        var $container = $self.parent();
 
         // Get the height of the container
         var totalHeight = $container[0].scrollHeight;
@@ -20,14 +19,14 @@ $(document).ready(function() {
         if(totalHeight <= $container.height()) {
             // Too small: does not require read-more
             $container.css('max-height', 'none');
-            $prompt.hide();
+            $self.hide();
         }
 
         $self.on('click', function() {
             // Set height to prevent instant jump-down when max height is removed
             $container.css({ "height": $container.height(), "max-height": 9999 }).animate({ "height": totalHeight });
 
-            $prompt.fadeOut(); // fade out read-more prompt
+            $self.fadeOut(); // fade out read-more prompt
 
             return false; // prevent jump-down
         });
